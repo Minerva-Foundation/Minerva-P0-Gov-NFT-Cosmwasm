692a33cc25ff357dc0426caac3342dd2d183fe31
Bela Bothin <s6beboth@gmail.com>
2022-01-18 22:27:46 +0100

    Remove unnecessary Metadata

--- contracts/cw721-metadata-onchain/src/lib.rs
+++ contracts/cw721-metadata-onchain/src/lib.rs
@@ -15,14 +15,9 @@ pub struct Trait {
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
 pub struct Metadata {
     pub image: Option<String>,
-    pub image_data: Option<String>,
     pub external_url: Option<String>,
     pub description: Option<String>,
     pub name: Option<String>,
-    pub attributes: Option<Vec<Trait>>,
-    pub background_color: Option<String>,
-    pub animation_url: Option<String>,
-    pub youtube_url: Option<String>,
 }
 
 pub type Extension = Option<Metadata>;
